d0c8fa95ac05dfc4fb7190260aa7a97fd5c6ed05
thayna <thaynas.s502@gmail.com>
2022-07-09 14:07:27 -0300

    first commit

---
+++ display_suggestions.js
@@ -0,0 +1,5 @@
+let display = document.createElement('div');
+function suggestions() {
+    display.setAttribute('id', 'suggestions');
+    document.getElementById('search').appendChild(display);
+}
